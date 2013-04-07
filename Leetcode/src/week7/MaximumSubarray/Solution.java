72b4b47d3e4ea7bf1ffb5112341019ecf49269fc
xiaoyangmie <juyuanyang@gmail.com>
2013-04-07 18:19:16 -0400

    Max Subarray, TODO: divide and conquer? in 2nd iteration

--- Leetcode/src/week7/MaximumSubarray/Solution.java
+++ Leetcode/src/week7/MaximumSubarray/Solution.java
@@ -15,21 +15,36 @@ package week7.MaximumSubarray;
 
 public class Solution {
 	public int maxSubArray(int[] A) {
-		if(A == null || A.length == 0) return 0;
+    	if(A == null || A.length == 0) return 0;
 		if(A.length == 1) return A[0];
-		int[] res = new int[A.length];
-		int max = A[0];
 		
+		int max = 0, cur = 0, maxItem = A[0];
 		for(int i = 0; i < A.length; i++){
-			res[i] = A[i];
-			if(A[i] > max) max = A[i];
-		}
-		for(int i = 1; i < A.length; i++){
-			for(int j = A.length - i - 1; j >= 0; j--){
-				res[j+i] = res[j+i-1] + A[j+i];
-				if(res[j+i] > max) max = res[j+i];
+			if(A[i] + cur >= 0) {
+				cur += A[i];
+				if(cur > max) max = cur;
 			}
+			else cur = 0;
+            if(maxItem < A[i]) maxItem = A[i];
 		}
+		
+        if(maxItem < 0) return maxItem;
 		return max;
+		
+		// Slow version
+//		int[] res = new int[A.length];
+//		int max = A[0];
+//		
+//		for(int i = 0; i < A.length; i++){
+//			res[i] = A[i];
+//			if(A[i] > max) max = A[i];
+//		}
+//		for(int i = 1; i < A.length; i++){
+//			for(int j = A.length - i - 1; j >= 0; j--){
+//				res[j+i] = res[j+i-1] + A[j+i];
+//				if(res[j+i] > max) max = res[j+i];
+//			}
+//		}
+//		return max;
 	}
 }
