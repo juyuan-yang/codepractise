a717b2c9926f12270690200be895534a1d059472
xiaoyangmie <juyuanyang@gmail.com>
2013-06-04 21:21:55 -0700

    3 sum

--- Leetcode/src/week11/ThreeSum/Solution.java
+++ Leetcode/src/week11/ThreeSum/Solution.java
@@ -16,6 +16,7 @@ The solution set must not contain duplicate triplets.
 package week11.ThreeSum;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 
 /**
@@ -26,48 +27,104 @@ import java.util.HashSet;
  * To change this template use File | Settings | File Templates.
  */
 public class Solution {
-    ArrayList<ArrayList<Integer>> res;
-    int[] temp;
+    // very tricky solution for me... very interesting!
     int[] num;
-    HashSet<ArrayList<Integer>> set;
     public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
-        res = new ArrayList<ArrayList<Integer>>();
-        temp = new int[3];
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
         this.num = num;
-        set = new HashSet<ArrayList<Integer>>();
-        if(num != null && num.length > 0 ){
-            tryEle(0, 0, 0);
+        if(num == null || num.length < 3) return res;
+        quicksort(0, num.length - 1);
+        System.out.println(num);
+        for(int i = 0; i < num.length - 3; i++){
+            if(i != 0 && num[i] == num[i-1]) continue;
+            int start = i+1, end=num.length-1;
+            while(start < end){
+                int total = num[i] + num[start] + num[end];
+                if(total == 0){
+                    ArrayList<Integer> item = new ArrayList<Integer>();
+                    item.add(num[i]);
+                    item.add(num[start]);
+                    item.add(num[end]);
+                    res.add(item);
+                    start++;
+                    end--;
+                    while(start < end && num[end] == num[end+1]) end--;
+                    while(start < end && num[start] == num[start-1]) start++;
+                } else if(total < 0){
+                    start++;
+                    while(start < end && num[start] == num[start-1]) start++;
+                } else {
+                    end--;
+                    while(start < end && num[end] == num[end+1]) end--;
+                }
+            }
         }
+
         return res;
     }
 
-    public void tryEle(int pos, int seq, int total){
-        if(pos == 3){
-            if(total != 0) return;
-
-            ArrayList<Integer> item = new ArrayList<Integer>();
-            item.add(temp[0]);
-            item.add(temp[1]);
-            item.add(temp[2]);
-            if(item.get(0) > item.get(1)) swap(0, 1, item);
-            if(item.get(1) > item.get(2)) swap(1, 2, item);
-            if(item.get(0) > item.get(1)) swap(0, 1, item);
-
-            if(!set.contains(item)) {
-                res.add(item);
-                set.add(item);
+    public void quicksort(int s, int t){
+        int i = s, j = t;
+        int temp = num[i];
+        while(i < j){
+            while(i < j && num[j] > temp) j--;
+            if(i < j){
+                num[i] = num[j];
+                i++;
             }
-            return;
+            while(i < j && num[i] < temp) i++;
+            if(i < j){
+                num[j] = num[i];
+                j--;
+            }
         }
-        for(int i = seq; i < num.length; i++){
-            temp[pos] = num[i];
-            tryEle(pos + 1, i + 1, total + num[i]);
-        }
+        num[i] = temp;
+        if(s < i - 1) quicksort(s, i-1);
+        if(i + 1 < t) quicksort(i+1, t);
     }
-
-    public void swap(int i, int j, ArrayList<Integer> item){
-        int t = item.get(i);
-        item.set(i, item.get(j));
-        item.set(j, t);
-    }
+    // time limit exceeds
+//    ArrayList<ArrayList<Integer>> res;
+//    int[] temp;
+//    int[] num;
+//    HashSet<ArrayList<Integer>> set;
+//    public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
+//        res = new ArrayList<ArrayList<Integer>>();
+//        temp = new int[3];
+//        this.num = num;
+//        set = new HashSet<ArrayList<Integer>>();
+//        if(num != null && num.length > 0 ){
+//            tryEle(0, 0, 0);
+//        }
+//        return res;
+//    }
+//
+//    public void tryEle(int pos, int seq, int total){
+//        if(pos == 3){
+//            if(total != 0) return;
+//
+//            ArrayList<Integer> item = new ArrayList<Integer>();
+//            item.add(temp[0]);
+//            item.add(temp[1]);
+//            item.add(temp[2]);
+//            if(item.get(0) > item.get(1)) swap(0, 1, item);
+//            if(item.get(1) > item.get(2)) swap(1, 2, item);
+//            if(item.get(0) > item.get(1)) swap(0, 1, item);
+//
+//            if(!set.contains(item)) {
+//                res.add(item);
+//                set.add(item);
+//            }
+//            return;
+//        }
+//        for(int i = seq; i < num.length; i++){
+//            temp[pos] = num[i];
+//            tryEle(pos + 1, i + 1, total + num[i]);
+//        }
+//    }
+//
+//    public void swap(int i, int j, ArrayList<Integer> item){
+//        int t = item.get(i);
+//        item.set(i, item.get(j));
+//        item.set(j, t);
+//    }
 }
