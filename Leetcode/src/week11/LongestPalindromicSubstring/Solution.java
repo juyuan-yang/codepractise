3f46eb3af4e90c61e074e08b30fc671906022536
xiaoyangmie <juyuanyang@gmail.com>
2013-06-05 20:53:41 -0700

    longest palindromic substring

--- Leetcode/src/week11/LongestPalindromicSubstring/Solution.java
+++ Leetcode/src/week11/LongestPalindromicSubstring/Solution.java
@@ -18,7 +18,33 @@ public class Solution {
     public String longestPalindrome(String s) {
         String res = "";
         if(s != null && s.length() > 0){
-
+            res += s.charAt(0);
+            int max = 1, t, mid = 0, length = 0;
+            boolean odd = true;
+            for(int i = 1; i < s.length() - 1; i++){
+                t = 1;
+                while(i-t >= 0 && i+t < s.length() && s.charAt(i-t) == s.charAt(i+t)) t++;
+                if(t*2 - 1 > max) {
+                    max = t*2 - 1;
+                    mid = i;
+                    length = t-1;
+                }
+            }
+            for(int i = 0; i < s.length() - 1; i++){
+                t = 1;
+                while(i-t+1 >= 0 && i+t < s.length() && s.charAt(i-t+1) == s.charAt(i+t)) t++;
+                if(t*2-2 > max) {
+                    odd = false;
+                    max = t*2 - 2;
+                    mid = i;
+                    length = t-1;
+                }
+            }
+            if(odd){
+                res = s.substring(mid-length, mid-length+max);
+            } else {
+                res = s.substring(mid-length+1, mid-length+1+max);
+            }
         }
         return res;
     }
