d7ae7d32745272483e6400c2b173c8fa50d58481
xiaoyangmie <juyuanyang@gmail.com>
2013-07-02 23:51:27 -0700

    Strstr, TODO: KMP

--- Leetcode/src/week12/ImplementeStrstr/Solution.java
+++ Leetcode/src/week12/ImplementeStrstr/Solution.java
@@ -8,7 +8,21 @@ Returns a pointer to the first occurrence of needle in haystack, or null if need
 package week12.ImplementeStrstr;
 
 public class Solution {
+    // TODO: implement KMP
     public String strStr(String haystack, String needle) {
+        if(haystack == null) return null;
+        if(needle == null || needle.length() == 0) return haystack;
+
+        for(int i = 0; i <= haystack.length() - needle.length(); i++){
+            int j = 0;
+            for( ; j < needle.length(); j++){
+                if(haystack.charAt(i+j) != needle.charAt(j)) break;
+            }
+            if(j == needle.length()){
+                return haystack.substring(i);
+            }
+        }
+
         return null;
     }
 }
