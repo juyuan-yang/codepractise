4b61e2a93e78b1ebf2dc852f868fb4acb53825ea
xiaoyangmie <juyuanyang@gmail.com>
2013-07-12 10:10:43 -0700

    time limit exceed version

--- Leetcode/src/week12/SubstringwithConcatenationofAllWords/Solution.java
+++ Leetcode/src/week12/SubstringwithConcatenationofAllWords/Solution.java
@@ -14,10 +14,68 @@ You should return the indices: [0,9].
 
 package week12.SubstringwithConcatenationofAllWords;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Solution {
+    boolean[] appear;
+    Map<Integer, ArrayList<Integer>> map;
+    Set<Integer> set;
+    ArrayList<Integer> res;
+    String S;
+    String[] L;
+
     public ArrayList<Integer> findSubstring(String S, String[] L) {
-        return null;
+        res = new ArrayList<Integer>();
+        if(S == null || S.length() == 0) return res;
+        if(L == null || L.length == 0) return res;
+        this.S = S;
+        this.L = L;
+
+        appear = new boolean[L.length];
+        // index of S, to words starting at this postion
+        map = new HashMap<Integer, ArrayList<Integer>>();
+        set = new HashSet<Integer>();
+
+        for(int i = 0; i < S.length(); i++){
+            ArrayList<Integer> list = new ArrayList<Integer>();
+            map.put(i, list);
+        }
+
+        for(int i = 0; i < L.length; i++){
+            if(i > 0 && L[i].equals(L[i-1])){
+                map.put(i, map.get(i-1));
+                continue;
+            }
+            int index = S.indexOf(L[i]);
+            while(index != -1){
+                map.get(index).add(i);
+                index = S.indexOf(L[i], index + L[i].length());
+            }
+        }
+
+        for(int i = 0; i <= S.length() - L.length * L[0].length(); i++){
+            if(map.get(i).size() > 0){
+                iter(0, i);
+            }
+        }
+
+        res.addAll(set);
+        return res;
+    }
+
+    public void iter(int total, int strIndex){
+        if(total == L.length){
+            set.add(strIndex - L.length * L[0].length());
+            return;
+        }
+        Set<String> dup = new HashSet<String>();
+        for(int index : map.get(strIndex)){
+            if(!appear[index] && !dup.contains(L[index])){
+                appear[index] = true;
+                iter(total + 1, strIndex + L[index].length());
+                appear[index] = false;
+                dup.add(L[index]);
+            }
+        }
     }
 }
